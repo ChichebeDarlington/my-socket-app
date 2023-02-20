66780bdb9dac2ee49b7dc559052ef82f05aa94bc
Chichebe <chichebewebdev@gmail.com>
2023-02-20 05:21:52 -0800

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,22 @@ const App = () => {
 
   const [username, setUsername] = useState("")
   const [room, setRoom] = useState("")
+  const [signin, setSignin] = useState(false)
 
   const startChart = ()=>{
     (username || room) && socket.emit("startChart", room)
+    setSignin(true)
   }
   return (
-    <div>
-      <Chat socket={socket} room={room} username={username}/>
-      <h3>chat app...</h3>
-      <input className="username" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-      <input className="room" placeholder="roomId" value={room} onChange={(e)=>setRoom(e.target.value)}/>
-      <button className="" onClick={startChart} >submit</button>
+    <div className="App">
+      <h3 className="App-header">chat app...</h3>
+      {
+        !signin?(<div className="sign">
+          <input className="username" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
+          <input className="room" placeholder="roomId" value={room} onChange={(e)=>setRoom(e.target.value)}/>
+          <button className="" onClick={startChart} >submit</button>
+          </div>):(<Chat socket={socket} room={room} username={username} />)
+      }
     </div>
   )
 }
--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {FaComment} from "react-icons/fa"
+import {FaPaperPlane} from "react-icons/fa"
+import scroll from "react-scroll-to-bottom"
 
 const Chat = ({socket,username,room}) => {
     const [currentMessage, setCurrentMessage]= useState("")
@@ -30,24 +31,28 @@ const Chat = ({socket,username,room}) => {
     },[socket])
 
   return (
-    <div>
+    <div className='chat-container'>
         <div className='chat-header'>chat live</div>
         <div className='chat-text'>
+            <scroll className="message-container">
             {messageList.map(messageChat=>{
-                const {author, date,message, roomId}=messageChat
+                const {author, date,message,signin, roomId}=messageChat
                 return <div>
                     <h3>{message}</h3>
                 </div>
             })}
+            </scroll>
         </div>
-        <div className='chat-footer'></div>
+        <div className='chat-footer'>
         <input 
+        className='chat-input'
         type="text" 
         placeholder='chat'
         value={currentMessage}
         onChange={(e)=>setCurrentMessage(e.target.value)}
         />
-        <button onClick={sendMessage}><FaComment/></button>
+        <button className='chat-btn' onClick={sendMessage}><FaPaperPlane/></button>
+        </div>
     </div>
   )
 }
